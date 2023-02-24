f66b988be87e954bfb77210af24075507cdba3a6
Siasmaan <asia.kittisak2@gmail.com>
2023-02-24 22:34:37 +0700

    new file:   Graph.py

---
+++ Graph.py
@@ -0,0 +1,7 @@
+class vertex:
+    def __init__(self, data, next):
+       print("hello world") #well i just gonna put this for make it have no error
+     
+class graph:
+    def __init__(self, vertex):
+        print("hello graph") #like the one above for no error
