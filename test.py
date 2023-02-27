d14d98835da6d48d54ad4e05390ce71a124570d7
Siasmaan <asia.kittisak2@gmail.com>
2023-02-27 23:19:42 +0700

    deleted:    queue.py 	modified:   test.py

--- test.py
+++ test.py
@@ -1,48 +1,20 @@
-class X(): 
-    def __init__(self, size): 
-        self.adj = [ [0] * size for i in range(size)]
-        self.size = size 
+def balanced(expression):
+    #your code goes here
+    bruh = expression
+    list = []
+    list.insert(0,bruh)
+    if list == []:
+        list.pop(0)
+    n = 0
+    for i in bruh:
+        if i == '(' or i == ')':
+            n += 1
+    if n % 2 == 0 and n > 0:
+        return "True"
+    elif n % 2 != 0:
+        return "False"
+    else:
+        return "False"
     
-    def add_friend(self, x, y): 
-        if x > self.size or y > self.size or x < 0 or y < 0: 
-            print("Error") 
-        else: 
-            self.adj[x-1][y-1] = 1 
-            self.adj[y-1][x-1] = 1 
-        
-    def remove_friend(self, x, y): 
-        if x > self.size or y > self.size or x < 0 or y < 0: 
-            print("Error") 
-        else: 
-            self.adj[x-1][y-1] = 0 
-            self.adj[y-1][x-1] = 0 
 
-    def display(self):
-        for row in self.adj:
-            print()
-            for val in row:
-                print(val, end="")
-                self.show = val
-            
-
-x = X(5)
-
-x.add_friend(1, 3) 
-x.add_friend(1, 5)
-x.add_friend(2, 5)
-x.add_friend(2, 4)
-x.add_friend(4, 5)
-
-n = int(input())
-#your code goes here
-
-print(x.display())
-sus = 0
-for z in range(len(x.adj)):
-    if z+1 == n:
-        for b in x.adj[z]:
-            sus+=1
-
-print(sus)   
-
-
+print(balanced(input()))
