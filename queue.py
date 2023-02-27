205ec82b43f38e48cb2a17860590905f4292f96a
Siasmaan <asia.kittisak2@gmail.com>
2023-02-27 21:14:53 +0700

    modified:   queue.py

--- queue.py
+++ queue.py
@@ -1,16 +1,15 @@
 class Queue:
     def __init__(self):
         self.items = []
-
     def is_empty(self):
         return self.items == []
-
-    def enqueue(self, item):
+    
+    def enqueue(slef, item):
         self.items.insert(0, item)
-
+    
     def dequeue(self):
         return self.items.pop()
-
+    
     def print_queue(self):
         print(self.items)
 
