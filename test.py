b02d2163730a39221b0315d5351b3dcb5ffeca7c
Siasmaan <asia.kittisak2@gmail.com>
2023-02-27 21:09:30 +0700

    new file:   queue.py 	new file:   stack.py 	modified:   test.py

--- test.py
+++ test.py
@@ -1,36 +1,48 @@
-class Track:
-    def __init__(self, title, next):
-        self.title = title
-        self.next = next
+class X(): 
+    def __init__(self, size): 
+        self.adj = [ [0] * size for i in range(size)]
+        self.size = size 
+    
+    def add_friend(self, x, y): 
+        if x > self.size or y > self.size or x < 0 or y < 0: 
+            print("Error") 
+        else: 
+            self.adj[x-1][y-1] = 1 
+            self.adj[y-1][x-1] = 1 
+        
+    def remove_friend(self, x, y): 
+        if x > self.size or y > self.size or x < 0 or y < 0: 
+            print("Error") 
+        else: 
+            self.adj[x-1][y-1] = 0 
+            self.adj[y-1][x-1] = 0 
 
-class Player:
-    def __init__(self):
-        self.head = None
+    def display(self):
+        for row in self.adj:
+            print()
+            for val in row:
+                print(val, end="")
+                self.show = val
+            
 
-    def add(self, title):
-        if not self.head:
-            self.head = Track(title, None)
-            return
-        curr = self.head
-        while curr.next:
-            curr = curr.next
-        curr.next = Track(title, None)
+x = X(5)
 
-    def show(self):
-        n = self.head
-        while(n != None):
-            print(n.title)
-            n = n.next
-            if n == None:
-                break
+x.add_friend(1, 3) 
+x.add_friend(1, 5)
+x.add_friend(2, 5)
+x.add_friend(2, 4)
+x.add_friend(4, 5)
 
-p = Player()
-while True:
-    x = input()
-    if x == 'end':
-        break
-    p.add(x)
-
+n = int(input())
 #your code goes here
 
-p.show()
+print(x.display())
+sus = 0
+for z in range(len(x.adj)):
+    if z+1 == n:
+        for b in x.adj[z]:
+            sus+=1
+
+print(sus)   
+
+
